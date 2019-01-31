552d33474afe8cb2ce49f88a587aa3debde0319e
Brian Bansenauer <brianb@cascadia.edu>
2019-01-31 14:20:22 -0800

    a2-start

---
+++ Controllers/NapsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using bit285_assignment2_login.Models;
+
+
+namespace bit285_assignment2_login.Controllers
+{
+    public class NapsController : Controller
+    {
+
+        /* 
+         * 1) Account Info 
+        */
+        public IActionResult AccountInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AccountInfo(User account)
+        {
+            return RedirectToAction("PasswordInfo", "Naps", account);
+        }
+        /* 
+         * 2) Password Info 
+        */
+        [HttpGet]
+        public IActionResult PasswordInfo(User account)
+        {
+             return View(account);
+        }
+        [HttpPost]
+        public IActionResult PasswordInfo(User account, string dummy)
+        {
+
+            return RedirectToAction("SelectPassword", "Naps", account);
+        }
+        /* 
+         * 3) Select Password  
+        */
+        [HttpGet]
+        public IActionResult SelectPassword(User account)
+        {
+            return View(account);
+        }
+        [HttpPost]
+        public IActionResult SelectPassword(User account, string dummy)
+        {
+            return RedirectToAction("ConfirmAccount", "Naps", account);
+        }
+        /* 
+         * 4) Confirm Account 
+        */
+        [HttpGet]
+        public IActionResult ConfirmAccount(User account)
+        {
+            return View(account);
+        }
+        [HttpPost]
+        public IActionResult ConfirmAccount()
+        {
+            return RedirectToAction("Login");
+        }
+        /* 
+         * 5) Login 
+        */
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/PasswordSuggetionService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bit285_assignment2_login.Models
+{
+    public class PasswordSuggetionService
+    {
+        public string generatePassword(User user) { 
+            List<string> _passwordOptions = new List<string>();
+            int half = user.LastName.Length / 2;
+            _passwordOptions.Add(user.FavoriteColor.Substring(0, 2) + "-" +
+                                        user.FavoriteColor + "-" +
+                                    user.LastName);
+            _passwordOptions.Add(user.LastName + user.FavoriteColor);
+            _passwordOptions.Add(user.LastName.Substring(0, half) + "-" +
+                                    user.BirthYear + "-" +
+                                    user.LastName.Substring(half,half));
+            Random random = new Random();
+            int index = random.Next(0,_passwordOptions.Count);
+            return _passwordOptions[index];
+        }
+    }
+}
